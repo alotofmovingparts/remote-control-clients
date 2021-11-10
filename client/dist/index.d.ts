38ab51c29007e7861c4a9a782565fa4c21449a25
Seth Thompson <s3th.thompson@gmail.com>
2021-11-10 09:19:50 -0500

    update gitignore

---
+++ client/dist/index.d.ts
@@ -0,0 +1,12 @@
+export interface Message {
+    type: string;
+}
+export default class TouchControlClient extends EventTarget {
+    url: string;
+    ws?: WebSocket;
+    constructor(url: string);
+    connect(callback?: (err: Error | null, initialValues: any, clients: any) => void): void;
+    sendValue(name: string, value: any, callback?: (err?: Error | null) => void): void;
+    on(type: string, callback: EventListenerOrEventListenerObject | null, options?: boolean | AddEventListenerOptions): void;
+    off(type: string, callback: EventListenerOrEventListenerObject | null, options?: EventListenerOptions | boolean): void;
+}
