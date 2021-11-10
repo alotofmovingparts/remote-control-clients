f832ef2cbd4b9f06bbf07c6dc5db90c860cf669f
Seth Thompson <s3th.thompson@gmail.com>
2021-11-10 11:13:43 -0500

    add state

--- client/dist/index.d.ts
+++ client/dist/index.d.ts
@@ -1,12 +1,19 @@
 export interface Message {
     type: string;
 }
+declare enum TouchControlClientState {
+    CONNECTING = 0,
+    OPEN = 1,
+    CLOSED = 2
+}
 export default class TouchControlClient extends EventTarget {
     url: string;
     ws?: WebSocket;
+    state: TouchControlClientState;
     constructor(url: string);
     connect(callback?: (err: Error | null, initialValues: any, clients: any) => void): void;
     sendValue(name: string, value: any, callback?: (err?: Error | null) => void): void;
     on(type: string, callback: EventListenerOrEventListenerObject | null, options?: boolean | AddEventListenerOptions): void;
     off(type: string, callback: EventListenerOrEventListenerObject | null, options?: EventListenerOptions | boolean): void;
 }
+export {};
